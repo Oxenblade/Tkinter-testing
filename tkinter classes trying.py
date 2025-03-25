be396d16c4fd92f7451cc1ff8088752e12b581e6
Yoshitha Ekanayake <hemiwysr07rox@gmail.com>
2025-03-25 19:07:10 +0530

    Add files via upload

---
+++ tkinter classes trying.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+root = Tk()
+root.title("project 01")
+root.iconbitmap("C:/Users/hp/Downloads/New folder (4)/python_104451.ico")
+root.geometry("500x500")
+
+class elder:
+    #root
+    def clicker(self):
+        print("look at you.. you clicked a button!")
+        
+    def _init_(self, master):
+        myframe = Frame(master)
+        myframe.pack()
+
+        self.mybutton = Button( master, text="Click", command = self.clicker)
+        self.mybutton.pack(pady=20)
+
+  
+
+
+        
+e = elder(root)
+
+
+
+root.mainloop()
